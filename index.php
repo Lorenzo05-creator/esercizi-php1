9174b54bb905effbbfed770f643f8d6549b3b686
Lorenzo Anastasio <lory4jz@gmail.com>
2025-04-15 12:21:14 +0200

    esercizio finito

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+/*Traccia 1:
+Installare PHP
+Definisci 4 variabili:
+Integer
+Float
+String
+Boolean
+A schermo, fai comparire il tipo di dato delle varie variabili.
+Trasforma quelle variabili in costanti, utilizzando le best practice viste a lezione.
+Pusha il codice su GitHub con il nome php_01_nome_cognome. Integra anche le altre tracce e poi pusha tutto.*/
+
+/*$integer = 69;
+$float = 69.9;
+$string = "Gianni";
+$boolean = true;*/
+
+const INTERO = 69;
+const DECIMALE = 69.9;
+const STRINGA = "stringa";
+const BOOLEANO = true;
+
+var_dump(INTERO);
+var_dump(DECIMALE);
+var_dump(STRINGA);
+var_dump(BOOLEANO);
+
+?>
